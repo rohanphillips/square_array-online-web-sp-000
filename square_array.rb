3e48711eb90092c2ad911a4c01767fcd0cad923f
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 18:56:23 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
+def square_array1(array)
+  newarray = []
+  x = 0
+  array.each do
+    newarray.push(array[x]**2)
+    x += 1
+  end
+
+end
+
 def square_array(array)
-  # your code here
+  newarray = []
+  x = 0
+  array.each do
+    newarray.push(array[x]**2)
+    x += 1
+  end
+  return newarray
+  #array.collect{|num| num ** 2}
 end
